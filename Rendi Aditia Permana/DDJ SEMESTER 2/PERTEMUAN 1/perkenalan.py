759ae7b72de290e7f6adeadcae691e9b27170712
rendiaditia15 <140995682+rendiaditia15@users.noreply.github.com>
2024-01-15 09:42:58 +0700

    Add files via upload

---
+++ Rendi Aditia Permana/DDJ SEMESTER 2/PERTEMUAN 1/perkenalan.py
@@ -0,0 +1,6 @@
+nama = "Rendi Aditia Permana"
+kelas = "10"
+jurusan = "Teknik Komputer Jaringan"
+
+print("perkenalkan nama saya", Rendi Aditia Permana)
+print("saya kelas", 10, "dari jurusan", Teknik Komputer Jaringan)
---
+++ Rendi Aditia Permana/DDJ SEMESTER 2/PERTEMUAN 1/variable.py
@@ -0,0 +1,10 @@
+x = 7
+y = 2
+z = "SMK MEDIKACOM"
+p = False
+q = True
+
+print(x)
+
+print("cek tipe data")
+print(type(x))
